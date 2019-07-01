ecbaa3cdcc80f8c68215e8febab3d2279645a9a1
Oluwole Ilesanmi <oluwoleilesanmi@gmail.com>
2019-07-01 16:01:41 +0100

    Configured project to use jquery + webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,15 @@
 const path = require("path");
+const webpack = require("webpack");
 
 module.exports = {
   entry: "./src/index.js",
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist")
-  }
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      $: "jquery"
+    })
+  ]
 };
