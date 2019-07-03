b1359ba82c23859a510142e1677c66bfb51d33ef
Oluwole Ilesanmi <oluwoleilesanmi@gmail.com>
2019-07-03 22:47:09 +0100

    Completed feature to add and ddisplay tasks

--- src/index.js
+++ src/index.js
@@ -1,1 +1,44 @@
 import "jquery";
+import { uuid } from "./js/util/uuid";
+
+let allTasks = [];
+
+let insert = (id, name, date) => {
+  allTasks.push({ id: id, name: name, date: date, done: false });
+};
+
+let li = task => {
+  return (
+    "" +
+    '<li todo-id = "' +
+    task.id +
+    '">' +
+    '<input type="checkbox" />' +
+    task.name +
+    task.date +
+    '<button class="task-del">-</button>' +
+    "</li>"
+  );
+};
+
+let render = () => {
+  let str = "";
+  allTasks.forEach(e => {
+    str += li(e);
+  });
+  $(".task-list").html(str);
+};
+
+let getTask = form => {
+  let t = form.elements[0].value;
+  let d = form.elements[1].value;
+  insert(uuid(), t, d);
+  render();
+};
+
+$(document).ready(function() {
+  $("form.task-entry").submit(function(e) {
+    e.preventDefault();
+    getTask(this);
+  });
+});
