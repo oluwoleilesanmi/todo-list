1047b3ba9f9c1862b2fa8512f5de4f02dd19fade
Oluwole Ilesanmi <oluwoleilesanmi@gmail.com>
2019-07-09 18:21:25 +0100

    Creating netlify build VI

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,12 @@
 const path = require("path");
 const webpack = require("webpack");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+// const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
   output: {
     filename: "main.js",
-    path: path.resolve(__dirname, "build")
+    path: path.resolve(__dirname, "dist")
   },
   module: {
     rules: [
@@ -17,19 +17,13 @@ module.exports = {
     ]
   },
   devServer: {
-    contentBase: path.join(__dirname, "build"),
+    contentBase: path.join(__dirname, "dist"),
     watchContentBase: true,
     port: 9000
   },
   plugins: [
     new webpack.ProvidePlugin({
       $: "jquery"
-    }),
-    new HtmlWebpackPlugin({
-      // injects main.js to our new index.html
-      inject: true,
-      // copys the content of the existing index.html to the new /build index.html
-      template: path.resolve("./dist/index.html")
     })
   ]
 };
